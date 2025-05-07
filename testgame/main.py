61897434ee1fdd12e157773ddbca4c21b4881561
unknown <oliverkilley12@gmail.com>
2025-05-07 18:11:37 +0100

    simple 2d game, basic controls, can exit via ESC

---
+++ testgame/main.py
@@ -0,0 +1,40 @@
+import pygame
+
+RESOLUTION = (1920, 1080)
+print(RESOLUTION)
+print(type(RESOLUTION))
+pygame.init()
+screen = pygame.display.set_mode()
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("blue")
+    
+    pygame.draw.circle(screen, "pink", player_pos, 40)
+    
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+        
+    if keys[pygame.K_ESCAPE]:
+        running = False
+            
+    pygame.display.flip()
+    
+    dt = clock.tick(60) / 1000
+    
+pygame.quit()
